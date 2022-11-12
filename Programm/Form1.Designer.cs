fc0905f0db1a21c4cb80fc98bbaeaad3951f12a0
MOPJAKA <vetal.bylga35@gmail.com>
2022-11-12 21:40:35 +0300

    Add programm

---
+++ Programm/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Programm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1292, 581);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.UseWaitCursor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newFileToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1337, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.UseWaitCursor = true;
+            // 
+            // newFileToolStripMenuItem
+            // 
+            this.newFileToolStripMenuItem.Name = "newFileToolStripMenuItem";
+            this.newFileToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.newFileToolStripMenuItem.Text = "New File";
+            this.newFileToolStripMenuItem.Click += new System.EventHandler(this.newFileToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Images|*.bmp;*.png;*.jpg";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Images|*.bmp;*.png;*.jpg";
+            this.saveFileDialog1.Title = "Сохранить изображение как...";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AllowDrop = true;
+            this.trackBar1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.trackBar1.Location = new System.Drawing.Point(1292, 24);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 581);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar1.UseWaitCursor = true;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1337, 605);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pix";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ Programm/pic.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+
+namespace Programm
+{
+    internal class pic
+    {
+        private int _width;
+        private int _height;
+        private Color[,] _pixel;
+
+        public pic()
+        {
+            _width = 0;
+            _height = 0;
+        }
+
+        public pic(Image orig)
+        {
+            Bitmap Borig = new Bitmap(orig);
+            _width = orig.Width;
+            _height = orig.Height;
+            _pixel = new Color[_width, _height];
+            Filling(Borig);
+        }
+
+        private void Filling(System.Drawing.Bitmap Borig)
+        {
+            for (int i = 0; i < Borig.Width; i++)
+            {
+                for (int j = 0; j < Borig.Height; j++)
+                {
+                    _pixel[i, j] = Borig.GetPixel(i, j);
+                    if (_pixel[i, j].A == 0)
+                    {
+                        _pixel = null;
+                        MessageBox.Show("Изображение не должно содержать пустых пикселей");
+                        return;
+                    }
+                }
+            }
+        }
+
+        public void Pixilization(Image Original, List<Bitmap> _bitmap)
+        {
+            Bitmap Bitmap_Original = new Bitmap(Original);
+            
+            int r, g, b, K;
+            if (Original.Width + Original.Height <= 1000)
+                K = 2;
+            else if (Original.Height + Original.Width <= 3500)
+                K = 4;
+            else
+                K = 8;
+            int width_O = 0, height_O = 0, width_P, height_P;
+
+            for (int n = 1; n < 11; n++)
+            {
+                width_P = Original.Width / K;
+                height_P = Original.Height / K;
+                _bitmap.Add(new Bitmap(width_P * K, height_P * K));
+                width_P = 0;
+                height_P = 0;
+                while (width_O < _width && width_O + K < _width)
+                {
+                    while (height_O < _height && height_O + K < _height)
+                    {
+                        r = g = b = 0;
+                        for (int m = width_O; m <= width_O + K; m++)
+                        {
+                            for (int l = height_O; l <= height_O + K; l++)
+                            {
+                                r += _pixel[m, l].R;
+                                g += _pixel[m, l].G;
+                                b += _pixel[m, l].B;
+                            }
+                        }
+                        r /= (K + 1) * (K + 1);
+                        g /= (K + 1) * (K + 1);
+                        b /= (K + 1) * (K + 1);
+
+                        for (int m = width_O; m < width_O + K; m++)
+                        {
+                            for (int l = height_O; l < height_O + K; l++)
+                            {
+                                _bitmap[n].SetPixel(m, l, Color.FromArgb(255, r, g, b));
+                            }
+                        }
+                        height_P += K;
+                        height_O += K;
+                    }
+                    width_P += K;
+                    height_P = 0;
+                    width_O += K;
+                    height_O = 0;
+                }
+                width_O = 0;
+                K = K * 4 / 3;
+            }
+        }
+    }
+}
